2fb4ad0fc40d4d4a39315dc186fc02e525260048
teriyakisource <vassilisnin@gmail.com>
2020-09-23 21:02:58 +0100

    removed unnescessary functions

--- main.py
+++ main.py
@@ -8,34 +8,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-backend = default_backend()
-iterations = 100_000
-
-
-def _derive_key(password: bytes, salt: bytes, iterations: int = iterations) -> bytes:
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(), length=32, salt=salt,
-        iterations=iterations, backend=backend)
-    return b64e(kdf.derive(password))
-
-def password_encrypt(message: bytes, password: str, iterations: int = iterations) -> bytes:
-    salt = secrets.token_bytes(16)
-    key = _derive_key(password.encode(), salt, iterations)
-    return b64e(
-        b'%b%b%b' % (
-            salt,
-            iterations.to_bytes(4, 'big'),
-            b64d(Fernet(key).encrypt(message)),
-        )
-    )
-
-def password_decrypt(token: bytes, password: str) -> bytes:
-    decoded = b64d(token)
-    salt, iter, token = decoded[:16], decoded[16:20], b64e(decoded[20:])
-    iterations = int.from_bytes(iter, 'big')
-    key = _derive_key(password.encode(), salt, iterations)
-    return Fernet(key).decrypt(token)
-
 def generate_key():
     key = Fernet.generate_key()
     with open("secret.key", "wb") as key_file:
